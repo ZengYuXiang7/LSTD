4467bcc4286be486cfe36e6ed43060a4ce0304ed
zengyuxiang <397125932@qq.com>
2024-11-04 20:46:11 +0800

    try

--- git.py
+++ git.py
@@ -13,19 +13,19 @@ def git_first_push(url):
     subprocess.run(f"git config --global credential.helper store", shell=True)
     subprocess.run(f"git push -u origin main", shell=True)
 
-
 def git_push(message):
     subprocess.run(f'git commit -am "{message}"', shell=True)
     subprocess.run(f"git push", shell=True)
 
 def git_pull():
-    subprocess.run(f'git commit -am "Commit the work before the pull"', shell=True)
     subprocess.run(f'git pull', shell=True)
 
+# def git_pull():
+#     subprocess.run(f'git commit -am "Commit the work before the pull"', shell=True)
+#     subprocess.run(f'git pull', shell=True)
 
 def git_reset(cnt):
     subprocess.run(f"git reset main{'^' * cnt}", shell=True)
-
 
 
 if __name__ == "__main__":
